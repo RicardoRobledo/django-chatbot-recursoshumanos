4023c22702e785a578fbd146fa7e84429f101da5
Ricardo Robledo <ricardoprogrammer44@gmail.com>
2024-02-20 19:01:22 -0600

    feat(main): actualizado a produccion

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,9 +1,15 @@
 from .base import *
+import sys
 
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'django-chatbot-recursoshumanos.onrender.com']
+
+
+__import__('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 
 DATABASES = {
     'default': {
